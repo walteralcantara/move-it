ce424757b361982ed569e27618c0c0f210b0b92f
Walter Alcantara <waltermalcantara@gmail.com>
2021-02-23 09:34:29 -0300

    Separating CSS into modules

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 
+import styles from '../styles/components/ExperienceBar.module.css';
+
 const ExperienceBar = () => {
     return (
-        <header className="experience-bar">
+        <header className={styles.experienceBar}>
+
             <span> 0 xp</span>
+
             <div>
                 <div style={{width: '50%'}} />
 
-                <span className="current-experience" style={{left: '50%'}}>
+                <span className={styles.currentExperience} style={{left: '50%'}}>
                     300xp
                 </span>
             </div>
+            
             <span> 600 xp</span>
 
         </header>
